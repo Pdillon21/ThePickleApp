dfbdf25b5137cb48a5b07753c078ee8c81558f50
Patricio Dillon <patricio.dillon@naranja.com>
2023-08-27 23:16:04 +0200

    EmptyLoadingScreen - EmptyLoadingScreen is remade to implement a custom lottie animation

--- app/src/main/java/com/example/thepickleapp/presentation/common_views/EmptyLoadingScreen.kt
+++ app/src/main/java/com/example/thepickleapp/presentation/common_views/EmptyLoadingScreen.kt
@@ -1,18 +1,46 @@
 package com.example.thepickleapp.presentation.common_views
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.thepickleapp.R
 
 @Composable
 fun EmptyLoadingScreen(modifier: Modifier = Modifier) {
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.wldd)
+    )
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        isPlaying = true,
+        iterations = LottieConstants.IterateForever
+    )
     Box(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        CircularProgressIndicator()
+        LottieAnimation(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+                .padding(24.dp)
+                .animateContentSize()
+                .padding(horizontal = 24.dp),
+            composition = composition,
+            progress = { progress }
+        )
     }
 }
