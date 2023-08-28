9bb06072a6343bc7159e7a55a4d1986d07add3a4
Patricio Dillon <patricio.dillon@naranja.com>
2023-08-28 20:36:00 +0200

    Paging Error View - The PagingErrorView is created - Paging cell is relocated to the list_cells directory.

--- app/src/main/java/com/example/thepickleapp/presentation/screens/main_screen/MainListView.kt
+++ app/src/main/java/com/example/thepickleapp/presentation/screens/main_screen/MainListView.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -14,7 +13,8 @@ import com.example.thepickleapp.data.dao.LocationDao
 import com.example.thepickleapp.data.dao.PagingError
 import com.example.thepickleapp.data.dao.PickleResultBase
 import com.example.thepickleapp.presentation.common_views.general.EndOfListView
-import com.example.thepickleapp.presentation.common_views.general.PagingCell
+import com.example.thepickleapp.presentation.common_views.list_cells.PagingCell
+import com.example.thepickleapp.presentation.common_views.general.PagingErrorView
 import com.example.thepickleapp.presentation.common_views.list_cells.CharacterColumnCell
 import com.example.thepickleapp.presentation.common_views.list_cells.EpisodeColumnCell
 import com.example.thepickleapp.presentation.common_views.list_cells.LocationColumnCell
@@ -50,7 +50,7 @@ fun MainListView(
                 }
 
                 is PagingError -> {
-                    Text(text = "ErrorPaging")
+                    PagingErrorView(item)
                 }
             }
         }
